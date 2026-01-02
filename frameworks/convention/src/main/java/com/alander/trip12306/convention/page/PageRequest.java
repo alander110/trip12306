7faa09450665fef9d91a086f48967e102e3cd393
alander110 <1366368016@qq.com>
2026-01-02 16:35:43 +0800

    feat(convention): 创建基础项目规范模块
    - 新增基础错误码、异常处理、分页、结果封装等通用类
    - 定义了客户端异常、服务端异常和远程服务调用异常
    - 实现了统一的错误码接口和基础错误码枚举
    - 添加了分页请求和响应类
    - 创建了全局返回结果类
    - 配置了 Maven 项目对象模型

---
+++ frameworks/convention/src/main/java/com/alander/trip12306/convention/page/PageRequest.java
@@ -0,0 +1,20 @@
+package com.alander.trip12306.convention.page;
+
+import lombok.Data;
+
+/**
+ * 分页请求对象
+ */
+@Data
+public class PageRequest {
+
+    /**
+     * 当前页
+     */
+    private Long current = 1L;
+
+    /**
+     * 每页显示条数
+     */
+    private Long size = 10L;
+}
